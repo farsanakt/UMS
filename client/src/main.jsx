3a7a1533395803a6b0562f623ffd4181b4f50375
Farsana kt <ffarsanakt@gmail.com>
2024-09-03 11:29:44 +0530

    add redux persit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { store } from './redux/store.js'
-import {Provider} from 'react-redux'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/es/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-)
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
